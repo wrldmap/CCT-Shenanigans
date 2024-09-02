d2c866955714de3ee15ac09047e1e66893fc4c30
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-09-02 10:49:21 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
 local decoder = dfpwm.make_decoder()
-for chunk in io.lines("lady.dfpwm", 128 * 1024) do
+for chunk in io.lines("lady.dfpwm", 32 * 1024) do
     local buffer = decoder(chunk)
 
     while not speaker.playAudio(buffer) do
