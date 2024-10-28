2bcad2188b6570ab0f115e3d9e01cdc430d9058b
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-10-28 17:52:58 -0500

    Create rand.lua

---
+++ rand.lua
@@ -0,0 +1,9 @@
+local num1 = math.random(1000)
+local num2 = math.random(1000)
+local num3 = math.random(1000)
+local num = num1+num2+num3
+print(num)
+
+if num = rand then
+  print("you won!!!")
+end
