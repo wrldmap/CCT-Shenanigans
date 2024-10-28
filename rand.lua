0bacecc361cc7e937174f8d986547b5cd7e37eb5
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-10-28 18:17:45 -0500

    Update rand.lua

--- rand.lua
+++ rand.lua
@@ -1,15 +1,17 @@
-local num1 = math.random(1000)
-local num2 = math.random(1000)
-local num3 = math.random(1000)
-local rand = math.random(3000)
-local num = num1+num2+num3
-print(num)
-print("")
-print("")
-print("")
+while true do
+    local num1 = math.random(100)
+    local num2 = math.random(100)
+    local num3 = math.random(100)
+    local win = 274
+    local num = num1+num2+num3
+    print(num)
+    print("")
+    if num == win then
+        print("you won!!!")
+    else
+        print("you lost :(")
+    end
 
-if num == rand then
-  print("you won!!!")
-else
-  print("you lost :(")
+    os.pullEvent("redstone")
+    sleep(1.5)
 end
