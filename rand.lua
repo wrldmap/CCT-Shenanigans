47904826352ec39d177f0ebd07c059456f5dc677
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-10-28 18:21:53 -0500

    Update rand.lua

--- rand.lua
+++ rand.lua
@@ -1,9 +1,6 @@
 while true do
-    local num1 = math.random(100)
-    local num2 = math.random(100)
-    local num3 = math.random(100)
-    local win = 274
-    local num = num1+num2+num3
+    local num = math.random(100)
+    local win = 28
     print(num)
     print("")
     if num == win then
